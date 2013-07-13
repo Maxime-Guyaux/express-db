e9f9e9ec4c0b8b226f3d3371ee01cb92196b416a
Stephane P. Pericat <web@pericat.net>
2013-07-13 16:09:15 +0200

    NEW basic db storage system

--- demo/app.js
+++ demo/app.js
@@ -7,6 +7,12 @@ var express = require('express'),
 
 	app.use(expressDB.init("demo DB"));
 
+	console.log("DB info: ", expressDB.info());
+
 	app.get('/', function(req, res) {
 		res.send("hello world!");
+		// expressDB.backup(function(err) {
+		// 	if (err) throw err;
+		// 	else console.log('db is backed up');
+		// });
 	});
