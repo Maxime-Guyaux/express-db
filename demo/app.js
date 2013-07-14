07df13cb7cc09c525f801e4528c85d55947bb653
Stephane P. Pericat <web@pericat.net>
2013-07-14 14:49:02 +0200

    view caching option

--- demo/app.js
+++ demo/app.js
@@ -9,7 +9,8 @@ app.use(xdb.init("demo DB", {
 	file: './bin/demo.db.json', //custom db file, if necessary
 	restrictAccess: false, //restrict access via browser
 	autoSave: true, //autosave enabled
-	backupInterval: 60000 //interval in ms
+	backupInterval: 60000 ,//interval in ms,
+	viewCaching: true
 }));
 
 //set
